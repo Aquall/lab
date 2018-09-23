69d80ce8bf9478aa000cc769c6beeb00b5871dcf
Nikita <nikitka_bodr@mail.ru>
2018-09-24 01:32:02 +0300

    one more upgrade

--- arrays/algorithms.h
+++ arrays/algorithms.h
@@ -1,9 +1,11 @@
 #ifndef ALGORITHMS_H_INCLUDED
 #define ALGORITHMS_H_INCLUDED
+using UnaryPredicate = bool(*)(int);
+using BinaryOperation = int(*)(int,int);
 namespace stu
 {
 template <typename T, typename UnaryPredicate>
-bool any_of(T*arr, int first, int last,UnaryPredicate p)
+bool any_of_(T*arr, int first, int last,UnaryPredicate p)
 {
     for(; first < last; ++first)
     {
@@ -15,7 +17,7 @@ bool any_of(T*arr, int first, int last,UnaryPredicate p)
     return false;
 }
 template <typename T, typename UnaryPredicate>
-bool all_of(T* arr, int first, int last,UnaryPredicate p)
+bool all_of_(T* arr, int first, int last,UnaryPredicate p)
 {
     for(; first < last; ++first)
     {
@@ -26,6 +28,35 @@ bool all_of(T* arr, int first, int last,UnaryPredicate p)
     }
     return true;
 
+}
+int all_of__(int* first,int* last,UnaryPredicate p)
+{
+    for(; first!=last; ++first)
+    {
+        if(!p(*first))
+            return false;
+    }
+    return true;
+}
+template <typename Iterator, typename UnaryPredicate>
+int all_of(Iterator first,Iterator last,UnaryPredicate p)
+{
+    for(; first!=last; ++first)
+    {
+        if(!p(*first))
+            return false;
+    }
+    return true;
+}
+template <typename T,typename BinaryOperation>
+int accumulate_(T* arr,int first, int last, T init, BinaryOperation op)
+{
+    for(; first < last; ++first)
+    {
+        init = op(init,arr[first]);
+
+    }
+    return init;
 }
 }
 
--- arrays/main.cpp
+++ arrays/main.cpp
@@ -2,10 +2,18 @@
 #include "algorithms.h"
 #include "predicates.h"
 using namespace std;
+using UnaryPredicate = bool(*)(int);
+using BinaryOperation = int(*)(int,int);
+namespace stu
+{
+
+}
 int main()
 {
     int arr[5] {6,4,-2,32,2};
-    std::cout << stu::all_of(arr,0,5,stu::is_even) << std::endl;
+    std::cout<<stu::all_of(arr,arr+5,stu::is_positive) << std::endl;
+    //cout << arr;
+    //std::cout << stu::accumulate_(arr,1,4,1000,[](int a, int b){return a + b;}) << std::endl;
 
     return 0;
 }
