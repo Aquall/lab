3422f6748aaff27018b98b29cb5d3fa8224ddd09
Nikita <nikitka_bodr@mail.ru>
2018-09-25 23:22:41 +0300

    lab3 3.3.3

---
+++ lab 3 3.3.3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "lab3.h"
+
+using namespace std;
+
+int main()
+{
+    cout << first_task(3) << endl;
+    cout << second_task(2,-2) << endl;
+    cout << third_task(-12345) << endl;
+    cout << fourth_task(124) << endl;
+    fifth_task();
+
+
+    return 0;
+}
