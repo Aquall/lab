3bab31a3471ab7eab94e3f4b85be1cf60522f459
Nikita <nikitka_bodr@mail.ru>
2018-10-10 22:54:41 +0300

    vorobei

---
+++ homework/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "task.h"
+
+using namespace std;
+int main()
+{
+    //cout << "f ="  << f(1,0) << endl;
+    //task3();
+    //cout << f4(5,1,1,1,f3) << endl;
+    cout << discriminant(2,4,2) << endl;
+
+    return 0;
+}
